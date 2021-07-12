58eb7a825b6561afaa44b85cf39cde19734f554c
indonswe <afc2002afc@hotmail.com>
2021-07-12 12:41:21 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+
+    }
+
+    public void methodIterate(List<Product> products,Conditional conditional,Action action){
+        
+
+    }
+
+}
